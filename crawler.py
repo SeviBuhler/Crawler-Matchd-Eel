38511e5da8b3129eaaa6faa5c47604b1dfbf6e19
SeviBuhler <sevi__buehler@hotmail.com>
2025-03-28 14:06:06 +0100

    added Method / Function to Crawler in  crawler.py: crawl_advision

--- crawler.py
+++ crawler.py
@@ -175,6 +175,9 @@ class Crawler:
                 elif 'app.ch/karriere' in current_url:
                     print(f"App: {'app.ch/karriere' in current_url}")
                     page_content, next_url = self.crawl_app(current_url, keywords)
+                elif 'advision.swiss' in current_url:
+                    print(f"Advision: {'advision.swiss' in current_url}")
+                    page_content, next_url = self.crawl_advision(current_url, keywords)
                 else:
                     print(f"Unknown URL: {current_url}")
                     return
@@ -2978,6 +2981,70 @@ class Crawler:
         except Exception as e:
             print(f"Error during crawl: {e}")
             return [], None
+          
+          
+    def crawl_advision(self, url, keywords):
+        """Function to crawl AdVision""" 
+        print(f"Crawling AdVision URL: {url}")
+        
+        try:
+            ### set up headers to mimic a browser
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.5,de;q=0.4',
+                'Accept-Encoding': 'identity',
+                'Connection': 'keep-alive',
+                'Upgrade-Insecure-Requests': '1',
+                'Sec-Fetch-Dest': 'document',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-Site': 'none',
+                'Sec-Fetch-User': '?1',
+            }
+            response = requests.get(url, headers=headers, timeout=10)
+            response.raise_for_status()
+            response.encoding = 'utf-8'
+            soup = BeautifulSoup(response.content, 'html.parser')
+            
+            job_section = soup.find('section', class_='job-list-area')
+            print(f"Job section: {job_section}")
+            job_rows = []
+            if job_section and isinstance(job_section, Tag):
+                print(f"Job section is valid. Looking for job rows.")
+                job_rows = job_section.find_all('div', class_='col-lg-6 col-md-6')
+            else:
+                print("Job section is not valid or not found.")
+            print(f"Found {len(job_rows)} job listings")
+            
+            content = []
+            for job in job_rows:
+                try:
+                    title = job.find('h3').text.strip()
+                    link = job.find('a')['href']
+                    location = 'Gossau'
+                    company = 'AdVision'
+                    
+                    ### Check if any keyword is in the title and job is an IT job
+                    if any(keyword.lower() in title.lower() for keyword in keywords) and self.is_it_job(title):
+                        content.append({
+                            'title': title,
+                            'link': link,
+                            'location': location,
+                            'company': company
+                        })
+                        print(f"Found matching job: {title}")
+                    else:
+                        print(f"Skipping non-matching job: {title}")
+                except Exception as e:
+                    print(f"Error during extraction: {e}")
+                    continue
+            next_page = None
+            print(f"Found {len(content)} jobs")
+            return content, next_page
+        
+        except Exception as e:
+            print(f"Error during crawl: {e}")
+            return [], None
             
                   
     
@@ -2992,6 +3059,6 @@ class Crawler:
 if __name__ == "__main__":
     ### Testing the crawler
     crawler = Crawler()
-    keywords = ['praktikum', 'werkstudent', 'praktika', 'frontend']
-    url = 'https://app.ch/karriere/stellenangebote'
+    keywords = ['praktikum', 'werkstudent', 'praktika', 'engineer']
+    url = 'https://advision.swiss/karriere/'
     crawler.crawl(url, keywords)
